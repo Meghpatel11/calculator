871729df9a22f45d7d5a615958c142df16c69334
megh <megh@gitgmail.com>
2022-07-22 14:57:03 -0700

    Added addition for double

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,3 @@
 Initial version of the Calculator project
+#2022-07-22
+Added integer addition function
--- Calaulator.java
+++ Calaulator.java
@@ -2,4 +2,7 @@ public class calculator(){
 	public int addition(int a,int b ){
 		return a+b; 
 	}
+
+	public double addition(double a,double b ){
+		return a+b; 
 }
